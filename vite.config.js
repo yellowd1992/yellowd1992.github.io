5ba0e07e5817f0fd3c64c92873dfb091f7439669
yellowd1992 <471291024@qq.com>
2026-03-10 21:18:23 +0800

    fix: 修正 base 路径为 /

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  // 用户站点 (username.github.io) 使用根路径
+  base: '/',
   server: {
     port: 3000,
     open: true
